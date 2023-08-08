54f8f8e042aa69805d251ef1df4f3ca3e3fc0753
SAMBIT PATRA <84149325+Sambitcr-7@users.noreply.github.com>
2023-08-08 05:41:17 +0530

    Time: 8 ms (11.62%), Space: 11.1 MB (45.03%) - LeetHub

---
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int low = 0,high = nums.size() - 1;
+        while(low <= high){
+            int mid = (low + high)/2;
+            if(nums[mid] == target){
+                return mid;
+            }
+            // identify the sorted half righ // left
+            // left sorted
+            if(nums[low] <= nums[mid]){
+                // target lies within you 
+                if(nums[low] <= target && target <= nums[mid]){
+                    // remove the righ half
+                    high = mid - 1;
+                }
+                else{
+                    low = mid + 1;
+                }
+            }
+
+            // righ sorted
+            else{
+                if(nums[mid] <= target && target <= nums[high]){
+                    // remove left half
+                    low = mid + 1;
+                }
+                else{
+                    high = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+};
