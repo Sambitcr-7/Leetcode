2291d1fdfa8a70dd7fbb897c7f5ca12984f2acb8
SAMBIT PATRA <84149325+Sambitcr-7@users.noreply.github.com>
2023-11-15 08:19:07 +0530

    Time: 71 ms (87.69%), Space: 51.5 MB (56.72%) - LeetHub

---
+++ 1846-maximum-element-after-decreasing-and-rearranging/1846-maximum-element-after-decreasing-and-rearranging.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maximumElementAfterDecrementingAndRearranging(vector<int>& arr) {
+         // sort the array to a non-decreasing order
+        sort(arr.begin(), arr.end());
+
+        // set the value of the first element as `1`
+        arr[0] = 1;
+
+        // iterate and make sure the rule (`abs(arr[i] - arr[i - 1]) <= 1`) meets
+        int result = 1;
+        for (auto& num : arr) result = min(result + 1, num);
+        return result;
+    }
+};
