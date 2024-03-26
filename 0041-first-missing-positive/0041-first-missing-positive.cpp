aeeafb702330273f2213af76c92b91c649579645
SAMBIT PATRA <84149325+Sambitcr-7@users.noreply.github.com>
2024-03-26 12:03:01 +0530

    Time: 43 ms (62.28%), Space: 43.6 MB (55.21%) - LeetHub

---
+++ 0041-first-missing-positive/0041-first-missing-positive.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int firstMissingPositive(vector<int>& nums) {
+        int n= size(nums);
+        for(int i=0;i<n;i++){
+            int x=nums[i];
+            while(x>0 && x<=n && nums[x-1]!=x){
+                swap(nums[x-1],nums[i]);
+                x=nums[i];
+            }
+        }
+        for(int i=0;i<n;i++)
+            if(nums[i] != i+1) return i+1;
+        return n+1;
+    }
+};
