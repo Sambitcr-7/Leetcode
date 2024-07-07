54153d85f1d229e88c72ac0c51b2b454bf0dbabb
SAMBIT PATRA <84149325+Sambitcr-7@users.noreply.github.com>
2024-07-07 08:37:32 +0530

    Time: 0 ms (100.00%), Space: 7.1 MB (72.30%) - LeetHub

---
+++ 1518-water-bottles/1518-water-bottles.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int numWaterBottles(int numBottles, int numExchange) {
+        int answer = 0 ;
+        int left = 0 ;
+        while(1){
+            answer = answer + numBottles ;
+            left = left + numBottles ;
+            numBottles = left / numExchange ;
+            left = left % numExchange ;
+            if(numBottles == 0){
+                break;
+            }
+        }
+        return answer ;
+    }
+};
