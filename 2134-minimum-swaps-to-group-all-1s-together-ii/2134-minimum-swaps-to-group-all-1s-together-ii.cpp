4baba51c8ecfc13f80d8e009a0fa49fc03ebb8a3
SAMBIT PATRA <84149325+Sambitcr-7@users.noreply.github.com>
2024-08-02 10:28:14 +0530

    Time: 53 ms (97.84%), Space: 93.4 MB (7.66%) - LeetHub

---
+++ 2134-minimum-swaps-to-group-all-1s-together-ii/2134-minimum-swaps-to-group-all-1s-together-ii.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int minSwaps(vector<int>& nums) {
+        //since array is circular, create a copy of the array and append it again to the end
+        //this trick works for all circular arrays
+        vector<int> copy(nums);
+        int count1 = 0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]==1){
+                count1++;
+            }
+            copy.push_back(nums[i]);
+        }
+        //sliding window of size(count1) for the first window
+        int cur=0;
+        for(int i=0;i<count1;i++){
+            if(copy[i]==1){
+                cur++;
+            }
+        }
+        int ans = count1-cur;
+        //sliding window for rest of the vector
+        for(int i=1;i<nums.size();i++){
+            if(copy[i-1]==1){
+                cur--;
+            }
+            if(copy[i+count1-1]==1){
+                cur++;
+            }
+            ans = min(ans,count1-cur);
+        }
+        return ans;
+    }
+};
