387de1c0a5e62e98a7fa2e567b5aebd90003f1ad
SAMBIT PATRA <84149325+Sambitcr-7@users.noreply.github.com>
2024-09-23 08:56:59 +0530

    Time: 93 ms (74.49%), Space: 69.1 MB (76.52%) - LeetHub

--- 2707-extra-characters-in-a-string/2707-extra-characters-in-a-string.cpp
+++ 2707-extra-characters-in-a-string/2707-extra-characters-in-a-string.cpp
@@ -1,35 +1,33 @@
 class Solution {
 public:
-    vector<int> dp;// dp[i]=the minimum extra characters needed for s[i:end]
     int n;
-    int f(int i, string& s, vector<string>& dictionary) {
-        if (i == n) return 0; // Base case
+    vector<int>dp;
+    unordered_set<string> st;
+    int solve(int idx, string &s)
+    {
+        if(idx == n) return 0;
 
-        if (dp[i]!= -1) return dp[i]; 
-        int ans = n; 
+        if(dp[idx] != -1) return dp[idx];
 
-        for (string& w : dictionary) {
-            int wLen = w.size();
-            if (i+wLen <= n && s.substr(i, wLen) == w) {
-            // If the current word matches the substring starting at position i
-                int extra = f(i+wLen, s, dictionary);
-                ans = min(ans, extra);
-            }
+        string cur;
+        int minimumExtra = n;
+        for(int i = idx; i < n; i++)
+        {
+            cur += s[i];
+
+            int nextExtra = solve(i + 1, s);
+            int curExtra = (st.count(cur)) ? 0 : cur.size();
+
+            minimumExtra = min(minimumExtra, curExtra + nextExtra);
         }
-
-        // The current character is not covered by any word in the dictionary
-        int noWord = 1 + f(i+1, s, dictionary);
-
-        // Take the minimum of using a word or not using a word
-        ans = min(ans, noWord);
-
-        return dp[i]=ans;
+        return dp[idx] = minimumExtra;
     }
-
-    int minExtraChar(string s, vector<string>& dictionary) {
-        n=s.size();
-        dp.assign(n, -1); 
-        return f(0, s, dictionary);
+    int minExtraChar(string s, vector<string>& dictionary) 
+    {
+        n = s.size();
+        dp.resize(55, -1);
+        for(auto &dict:dictionary)
+            st.insert(dict);
+        return solve(0, s);
     }
-
 };
